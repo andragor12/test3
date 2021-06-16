1391700ce21171e2914ecc556714a538bf97ca0e
Andy <andragor@gmail.com>
2021-06-16 16:51:41 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 const numberOfFilms
 numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?')
+
+let personalMovieDB {
+    'count': numberOfFilms,
+    'movies': '';
+'actors': '';
+'genres': '';
+'privat': false
+}
